f8e09338e02c12fc45a223d524e0879e8fe51c97
LarsBuecker <48998231+LarsBuecker@users.noreply.github.com>
2020-08-21 14:09:56 +0200

    DetailPage routing

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -83,25 +83,32 @@ class ToDoItem extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 22),
       child: ListTile(
-          contentPadding: EdgeInsets.symmetric(vertical: 8.0),
-          leading: Checkbox(
-            value: done,
-            onChanged: (bool value) => toggleDone(),
+        contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+        leading: Checkbox(
+          value: done,
+          onChanged: (bool value) => toggleDone(),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+            color: done ? Color.fromRGBO(23, 152, 185, 100) : Colors.black54,
+            decoration: done ? TextDecoration.lineThrough : TextDecoration.none,
           ),
-          title: Text(
-            title,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w600,
-              color: done ? Color.fromRGBO(23, 152, 185, 100) : Colors.black54,
-              decoration:
-                  done ? TextDecoration.lineThrough : TextDecoration.none,
-            ),
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.delete_outline),
-            onPressed: () => remove(),
-          )),
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.delete_outline),
+          onPressed: () => remove(),
+        ),
+        onTap: () {
+          Navigator.push<Widget>(
+              context,
+              MaterialPageRoute<Widget>(
+                  builder: (BuildContext context) =>
+                      DetailScreen(title, done)));
+        },
+      ),
     );
   }
 }
@@ -149,5 +156,54 @@ class _AddItemDialogState extends State<AddItemDialog> {
                 )
               ],
             )));
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen(this.title, this.done);
+  final String title;
+  final bool done;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: done ? Colors.green : Colors.red,
+      appBar: AppBar(
+        title: Text('Detail Screen'),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 70),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Expanded(
+                child: Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    child: Text(
+                      done
+                          ? 'Das hast du schon erledigt:'
+                          : 'Das musst du noch machen:',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ))),
+            Expanded(
+                child: Container(
+                    alignment: Alignment.center,
+                    child: Text(title,
+                        style: TextStyle(fontSize: 50, color: Colors.white)))),
+            Expanded(
+              child: IconButton(
+                iconSize: 60,
+                onPressed: () => Navigator.pop(context),
+                icon:
+                    Icon(done ? Icons.check : Icons.close, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
